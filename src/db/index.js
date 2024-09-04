962be250ffe798f9b3f84a80afcc5734b6e25b4e
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-04 21:05:22 +0530

    "Updated mongoose connection options in connectDB function"

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/QueryTesting`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/QueryTesting`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 45000
+        })
         console.log("\n Database connected, DB HOST: ", connectionInstance.connection.host);
 
     } catch (error) {
